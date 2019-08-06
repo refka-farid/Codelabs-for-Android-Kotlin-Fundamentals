21e233eed15a71c4e353ff0d040563700410f905
refka-farid <refka.farid@gmail.com>
2019-08-06 18:47:15 +0200

    [learning kts] add DiceRollerApp challenge solution

--- app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
+++ app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
@@ -9,25 +9,28 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultText = findViewById(R.id.result_text)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
         val countUpButton: Button = findViewById(R.id.count_up_button)
         countUpButton.setOnClickListener { countUp() }
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset() }
     }
+
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
     }
 
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
         if (resultText.text == "Hello Word") {
             resultText.text = "1"
         } else {
@@ -37,5 +40,9 @@ class MainActivity : AppCompatActivity() {
                 resultText.text = resultInt.toString()
             }
         }
+    }
+
+    private fun reset() {
+        resultText.text = "0"
     }
 }
