a751b7e171ec977ddd307c764e2612c7b5bbcb02
refka-farid <refka.farid@gmail.com>
2019-08-06 16:05:20 +0200

    [learning kts] wip DiceRollerApp

--- app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
+++ app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
@@ -15,12 +15,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "Hello Word") {
+            resultText.text = "1"
+        } else {
+            var resultInt = resultText.text.toString().toInt()
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
