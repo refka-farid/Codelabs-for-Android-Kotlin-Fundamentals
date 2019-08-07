3ca9889a8122bf00e7f7f3355991a36cacba4153
refka-farid <refka.farid@gmail.com>
2019-08-07 10:15:17 +0200

    [learning kts] : DiceRollerFinal-challenge

--- app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
+++ app/src/main/java/com/bravedroid/diceroller/MainActivity.kt
@@ -2,47 +2,40 @@ package com.bravedroid.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var resultText: TextView
+    private lateinit var diceImage1: ImageView
+    private lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultText = findViewById(R.id.result_text)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { reset() }
     }
 
 
     private fun rollDice() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int {
         val randomInt = Random().nextInt(6) + 1
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp() {
-        if (resultText.text == "Hello Word") {
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.text.toString().toInt()
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-    }
-
-    private fun reset() {
-        resultText.text = "0"
+        return drawableResource
     }
 }
